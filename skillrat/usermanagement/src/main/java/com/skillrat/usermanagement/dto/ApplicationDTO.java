1dd8953ae67b0afb034cb4dca91db02d385bb718
Chaithanya-95 <chaitanya.kancharla@juvarya.com>
2025-08-29 09:04:45 +0530

    DTO valiadations added

--- skillrat/usermanagement/src/main/java/com/skillrat/usermanagement/dto/ApplicationDTO.java
+++ skillrat/usermanagement/src/main/java/com/skillrat/usermanagement/dto/ApplicationDTO.java
@@ -1,6 +1,7 @@
 package com.skillrat.usermanagement.dto;
 
 import com.skillrat.usermanagement.dto.enums.ApplicationStatus;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,17 +14,24 @@ public class ApplicationDTO {
 
     private Long id;
 
+    @NotNull(message = "B2B Unit ID is required")
     private Long b2bUnitId;
 
+    @NotNull(message = "Requirement ID is required")
     private Long requirementId;
 
+    @NotNull(message = "Applicant User ID is required")
     private Long applicantUserId;
 
+    @NotNull(message = "Status is required")
     private ApplicationStatus status;
 
+    @Size(max = 2000, message = "Cover letter must not exceed 2000 characters")
     private String coverLetter;
 
-    private Set<MediaDTO> mediaFiles;
+    @NotNull(message = "At least one media file is required")
+    @Size(min = 1, message = "At least one media file must be provided")
+    private Set<@NotNull MediaDTO> mediaFiles;
 
     private LocalDateTime createdAt;
 
